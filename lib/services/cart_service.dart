7b75353fdc27fcb140f7a86a72b3c95e8b2f4e3b
Shreya-91pandey <sp327272@gmail.com>
2026-09-13 17:10:43 +0530

    Create CartService for cart management
    Implement CartService to manage shopping cart operations.

---
+++ lib/services/cart_service.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import '../models/cart_item.dart';
+import '../models/product.dart';
+
+class CartService extends ChangeNotifier {
+  final List<CartItem> _items = [];
+
+  List<CartItem> get items => List.unmodifiable(_items);
+  int get itemCount => _items.fold(0, (sum, item) => sum + item.quantity);
+  double get totalAmount =>
+      _items.fold(0.0, (sum, item) => sum + item.total);
+
+  void addToCart(Product product, {int quantity = 1}) {
+    final existing = _items.indexWhere((i) => i.product.id == product.id);
+    if (existing != -1) {
+      _items[existing].quantity += quantity;
+    } else {
+      _items.add(CartItem(product: product, quantity: quantity));
+    }
+    notifyListeners();
+  }
+
+  void removeFromCart(String productId) {
+    _items.removeWhere((i) => i.product.id == productId);
+    notifyListeners();
+  }
+
+  void updateQuantity(String productId, int quantity) {
+    final index = _items.indexWhere((i) => i.product.id == productId);
+    if (index != -1) {
+      if (quantity <= 0) {
+        _items.removeAt(index);
+      } else {
+        _items[index].quantity = quantity;
+      }
+      notifyListeners();
+    }
+  }
+
+  void clear() {
+    _items.clear();
+    notifyListeners();
+  }
+
+  bool isInCart(String productId) {
+    return _items.any((i) => i.product.id == productId);
+  }
+}
